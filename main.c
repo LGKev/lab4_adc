9f007d4e43dee707350bb8c9ecdea370cae2b0f5
Kevin Kuwata <kevinkuwata@icloud.com>
2017-10-22 17:39:15 -0600

    adc fully functional single channel

--- main.c
+++ main.c
@@ -1,27 +1,20 @@
 #include "msp.h"
 #include "adc_driver.h"
 #include "lab4.h"
-#include "core_cm4.h"
 
-#define SCB_SCR_ENABLE_SLEEPONEXIT      (0x00000002)
 /**
  * main.c
  */
-
 void main(void)
-
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
 #ifdef INTERNAL_TEMPERATURE_READ
+	ADC_CONFIG();
 #endif
-    ADC_CONFIG();
+
 	__enable_interrupt();
-
-
 	while(1){
 	    ADC14->CTL0 |= ADC14_CTL0_SC | ADC14_CTL0_ENC; //start sample, enable conversion?
-
 	}
 }
-
